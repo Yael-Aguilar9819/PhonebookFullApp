f5ee628cc1dc63089802696ed529fc5b88891090
Yael Aguilar <yaelgarcia9819@outlook.com>
2021-06-25 11:44:43 -0500

    changed mongo.js to reject different length of results

--- backend/mongo.js
+++ backend/mongo.js
@@ -1,32 +1,41 @@
 const mongoose = require('mongoose')
 
+//This is necesary because the only possible arguments are 3 if it's only the password arg
+//and only 4-5 if it's only the name or name with the number
 if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+  console.log('Please provide the password as an argument: node mongo.js <password> <person name> <person number>');
+  process.exit(1);
+} 
+else if (process.argv.length > 5) {
+  console.log('if the name contains whitespace, it must be enclosed in quotes: node mongo.js <password> "person name" <person number>');
+  process.exit(1);  
 }
 
 const password = process.argv[2]
 
-const url =
-  `mongodb+srv://fullstack:${password}@cluster0-ostce.mongodb.net/test?retryWrites=true`
+//this is the route if the arg it's only the pass
+// if 
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+// const url =
+//   `mongodb+srv://fullstack:${password}@cluster0-ostce.mongodb.net/test?retryWrites=true`
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-})
+// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
-const Note = mongoose.model('Note', noteSchema)
+// const noteSchema = new mongoose.Schema({
+//   content: String,
+//   date: Date,
+//   important: Boolean,
+// })
 
-const note = new Note({
-  content: 'HTML is Easy',
-  date: new Date(),
-  important: true,
-})
+// const Note = mongoose.model('Note', noteSchema)
 
-note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
-})
+// const note = new Note({
+//   content: 'HTML is Easy',
+//   date: new Date(),
+//   important: true,
+// })
+
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
